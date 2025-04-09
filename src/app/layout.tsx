6ffb1900233453d4dc20461aece594522761d09d
aditya6715 <adityakumar6715@gmail.com>
2025-04-09 15:40:33 +0530

    Fixed children type in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
-// src/app/layout.tsx
 import "./globals.css";
-import { Toaster } from "react-hot-toast";  
-import type { Metadata } from "next";
+import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-  title: "UX Design Masterclass",
-  description: "Learn top UX skills from industry experts",
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        <Toaster position="top-right" reverseOrder={false} />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
-
